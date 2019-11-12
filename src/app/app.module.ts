e531eb1f662b1a3f41ceb8fb2502edc279fda5ff
ghatfan99 <ghtfan99@hotmail.fr>
2019-11-12 17:48:42 +0100

    bureaux projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BureauxComponent } from './bureaux/bureaux.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BureauxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bureaux/bureaux.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bureaux',
+  templateUrl: './bureaux.component.html',
+  styleUrls: ['./bureaux.component.css']
+})
+export class BureauxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getOfficeId(event) {
+    console.log(event.target.id);
+  }
+}
